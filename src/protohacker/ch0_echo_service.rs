7da11635167599a02c07a1fb5ad0fcf66a0c3385
zwpdbh <zw>
2024-05-11 15:01:18 +0800

    > debug client read

--- src/protohacker/ch0_echo_service.rs
+++ src/protohacker/ch0_echo_service.rs
@@ -6,26 +6,19 @@ use tokio::io::AsyncWriteExt;
 use tokio::io::BufReader;
 use tokio::net::TcpListener;
 use tokio::net::TcpStream;
-use tracing::{error, info};
+use tracing::debug;
+use tracing::{error, info, warn};
 
 pub async fn server_run(port: u32) {
     let addr = format!("127.0.0.1:{}", port);
 
-    let socket = TcpListener::bind(&addr).await.unwrap();
+    let listener = TcpListener::bind(&addr).await.unwrap();
     info!("Listening on: {}", addr);
 
-    while let Ok((stream, peer)) = socket.accept().await {
+    while let Ok((stream, peer)) = listener.accept().await {
         info!("Incoming connection from: {}", peer);
         tokio::spawn(async move {
             let () = handle_connection(stream, peer).await;
-            // info!("thread starting {} starting", peer);
-
-            // let five_seconds = time::Duration::from_secs(5);
-            // let begin = time::Instant::now();
-            // let _ = tokio::time::sleep(five_seconds).await;
-
-            // let end = begin.elapsed();
-            // info!("thread {} finising {}", peer, end.as_secs_f32());
         });
     }
 }
@@ -45,21 +38,14 @@ async fn handle_connection(mut stream: TcpStream, peer: SocketAddr) {
             Ok(n) => {
                 // telling the reader to stop reading once it hits the EOF condition.
                 if n == 0 {
-                    info!("EOF received");
+                    warn!("EOF received");
                     break;
                 }
                 let buf_string = String::from_utf8_lossy(&buf);
-
-                let data: Vec<String> = buf_string
-                    .split(';')
-                    .map(|x| x.to_string().replace('\n', ""))
-                    .collect();
-
-                info!("Received message: {:?}", data);
+                info!("Received message: {:?}", buf_string);
 
                 // Echo back to the client
-                let response = format!("{:?}", data);
-                let _ = writer.write_all(response.as_bytes()).await;
+                let _ = writer.write_all(buf_string.as_bytes()).await;
                 let _ = writer.flush().await;
 
                 buf.clear()
@@ -76,8 +62,41 @@ async fn handle_connection(mut stream: TcpStream, peer: SocketAddr) {
 
 pub async fn client_run(port: u32) -> Result<(), Box<dyn Error>> {
     let mut stream = TcpStream::connect(format!("127.0.0.1:{}", port)).await?;
+    let (reader, mut writer) = stream.split();
+
     info!("stream starting");
-    stream.write_all(b"hello world").await?;
+
+    let _ = writer.write_all(b"hello world").await?;
+    let _ = writer.flush().await; // Never forget to do this
+
+    let mut buf_reader = BufReader::new(reader);
+    let mut buf = vec![];
+
+    loop {
+        debug!("why this doesn't work");
+        match buf_reader.read_until(b'\n', &mut buf).await {
+            Ok(n) => {
+                // telling the reader to stop reading once it hits the EOF condition.
+                if n == 0 {
+                    info!("EOF received");
+                    break;
+                }
+                let buf_string = String::from_utf8_lossy(&buf);
+
+                let data: Vec<String> = buf_string
+                    .split(';')
+                    .map(|x| x.to_string().replace('\n', ""))
+                    .collect();
+
+                info!("Received message: {:?}", data);
+
+                buf.clear();
+            }
+            Err(e) => {
+                error!("Error receiving message: {}", e)
+            }
+        }
+    }
 
     info!("stream finished");
     Ok(())
